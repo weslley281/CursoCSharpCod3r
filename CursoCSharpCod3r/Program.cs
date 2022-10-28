8ecd9838143a00c8734e18653f9ff428d2feab7c
Weslley Ferraz <weslleyhenrique800@gmail.com>
2022-10-27 20:20:53 -0400

    a

---
+++ CursoCSharpCod3r/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace CursoCSharpCod3r {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ CursoCSharpCod3r/fundamentos/PrimeiroPrograma.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharpCod3r.fundamentos {
+    class PrimeiroPrograma {
+    }
+}
